897ce78c4d24309d6a5930aec72d575ca4ac4b36
Benjamin Bach <benjaoming@gmail.com>
2016-10-14 20:25:36 +0200

    auto-creation of manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 
+local_py = os.path.join(os.path.dirname(__file__), "lcrsweb", "settings", "local.py")
+
+if not os.path.exists(local_py):
+    open(local_py, "w").write("from .dev import *\n")
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcrsweb.settings")
 

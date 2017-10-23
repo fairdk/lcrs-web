41cc19a2b9c2e291d546591ea24d0fd8b8d271b4
Benjamin Bach <benjamin@overtag.dk>
2017-10-23 16:35:26 +0200

    Update to django 1.11 and Wagtail 1.13

--- lcrsweb/wsgi.py
+++ lcrsweb/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcrsweb.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcrsweb.settings")
 
 application = get_wsgi_application()
